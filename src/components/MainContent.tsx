1ab0f5921c0d45d6e73203a147efd0211a276a47
grace k <grace.kuperman5472@gmail.com>
2023-01-17 12:22:40 +0000

    ready to push

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,11 +2,19 @@ import HomePage from "./HomePage";
 import CatalogPage from "./CatalogPage";
 import { Route, Routes } from "react-router-dom";
 import SignInPage from "./SignInPage";
+import axios from "axios";
 
 function MainContent(): JSX.Element {
-  const fetchAndStoreUsers = async () => {
-    // axios.get
-  };
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://study-resource-catalog.onrender.com/"
+      : "http://localhost:4000";
+
+  //   const fetchAndStoreUsers = async () => {
+  //       const response = await axios.get(`${baseUrl}/users`)
+  //       const userData = response.data.data
+  //       const userArr = []
+  //   };
   return (
     <div>
       <Routes>
