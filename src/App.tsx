455270406852bae20cbbc9ffcc823802586c956c
Alessia B <borys.alessia@gmail.com>
2023-01-19 16:58:30 +0100

    struct done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 import React from "react";
+import HomePage from "./components/HomePage";
+import CatalogPage from "./components/CatalogPage";
+import { Route, Routes } from "react-router-dom";
+import SignInPage from "./components/SignInPage";
+import axios from "axios";
+import { IUserData } from "./utils/interfaces";
+import { useCallback, useEffect, useState } from "react";
+import { url } from "./utils/url";
 import "./App.css";
 import NavBar from "./components/NavBar";
-// import HomePage from "./components/HomePage";
-import MainContent from "./components/MainContent";
 
 function App() {
+  const [users, setUsers] = useState<IUserData[]>([]);
+  const [signedInUser, setSignedInUser] = useState<string | undefined>();
+  //signedinuser to be used in top corner of navbar after it has been assigned ---> may need to pass this state from App down to SigninPage instead***
+
+  const fetchAndStoreUsers = useCallback(async () => {
+    const response = await axios.get(`${url}/users`);
+    let userData: IUserData[] = response.data;
+    console.log(userData);
+    setUsers(userData);
+  }, []);
+
+  useEffect(() => {
+    fetchAndStoreUsers();
+  }, [fetchAndStoreUsers]);
   return (
     <div className="App">
-      <NavBar />
-      <MainContent />
+      <NavBar signedInUser={signedInUser} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/catalog" element={<CatalogPage />} />
+
+        <Route
+          path="/signIn"
+          element={
+            <SignInPage IUserData={users} setSignedInUser={setSignedInUser} />
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
-
-function AppHeader(): JSX.Element {
+interface NavBarProps {
+  signedInUser: string | undefined;
+}
+function NavBar(props: NavBarProps): JSX.Element {
   return (
     <nav>
       <Link to="/"> HOME </Link>
       <Link to="/catalog"> CATALOG </Link>
       <Link to="/signIn"> SIGN-IN </Link>
+      {props.signedInUser !== undefined ? (
+        <p>{props.signedInUser}</p>
+      ) : (
+        <Link to="/signIn"> sign in please </Link>
+      )}
     </nav>
   );
 }
 
-export default AppHeader;
+export default NavBar;
