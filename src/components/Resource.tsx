54927563b96e906d1a00af6df16aed2f0b54128c
Alessia B <borys.alessia@gmail.com>
2023-01-20 14:24:40 +0100

    todo added

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -1,34 +1,36 @@
 import { IResourceData } from "../utils/interfaces";
 
 interface ResourceProps {
-    resourceData: IResourceData;
+  resourceData: IResourceData;
 }
 
-function Resource({resourceData}: ResourceProps): JSX.Element {
-    return (
-    <div className="ctn-resource"> 
-    <p className="resource-title">{resourceData.title}</p>
-    <p className="resource-post-date">{String(resourceData.post_date)}</p>
+//TODO: add conditional rendering to a button "FULL VIEW"
+//      if !FULL_VIEW =>  the current map (add comment and fullview btn)
+//      if  FULL_VIEW =>  all tingzz
 
-    {/* use state to render? */}
-    {resourceData.usage === 'rec used' && <p>🌟</p>}
-    {resourceData.usage === 'no rec used' && <p>💩</p>}
-    {resourceData.usage === 'not used promise' && <p>🔍</p>}
+function Resource({ resourceData }: ResourceProps): JSX.Element {
+  return (
+    <div className="ctn-resource">
+      <p className="resource-title">{resourceData.title}</p>
+      <p className="resource-post-date">{String(resourceData.post_date)}</p>
 
-    <p className="resource-type">{resourceData.type}</p>
-    <p className="resource-description">{resourceData.description}</p>
-    {resourceData.tags.map(tag => {
-        return (
-            <div className="resource-tag">
-                {tag}
-            </div>
-        )
-    })}
-    <div className="resource-link-btn"><a href={resourceData.link}>Check it out</a></div>
-    <button className="like-resource-btn">👍</button>
-    <button className="dislike-resource-btn">👎</button>
+      {/* use state to render? */}
+      {resourceData.usage === "rec used" && <p>🌟</p>}
+      {resourceData.usage === "no rec used" && <p>💩</p>}
+      {resourceData.usage === "not used promise" && <p>🔍</p>}
+
+      <p className="resource-type">{resourceData.type}</p>
+      <p className="resource-description">{resourceData.description}</p>
+      {resourceData.tags.map((tag) => {
+        return <div className="resource-tag">{tag}</div>;
+      })}
+      <div className="resource-link-btn">
+        <a href={resourceData.link}>Check it out</a>
+      </div>
+      <button className="like-resource-btn">👍</button>
+      <button className="dislike-resource-btn">👎</button>
     </div>
-    );
-  }
-  
-  export default Resource;
+  );
+}
+
+export default Resource;
