f69bf93104c4ad4343a9d47fa842c242ea3a5cb5
Alessia B <borys.alessia@gmail.com>
2023-01-23 15:06:32 +0100

    Comments POST and GET done

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -17,6 +17,7 @@ function Resource({
 }: ResourceProps): JSX.Element {
   const [isFullView, setIsFullView] = useState<boolean>(false);
   const [comments, setComments] = useState<ICommentData[]>();
+  const [newComment, setNewComment] = useState<string>("");
 
   //-----------------------------------get comments
   const handleTopLvCommentBtn = (id: number) => {
@@ -27,6 +28,27 @@ function Resource({
     let resourceComments: ICommentData[] = response.data;
     console.log(resourceComments);
     setComments(resourceComments);
+  };
+
+  //-----------------------------------leave a new comment
+  const handleSubmitNewComment = (commentTxt: string) => {
+    postNewComment(commentTxt).then(() => getResourceComments(resourceData.id));
+    setNewComment("");
+  };
+  const postNewComment = async (commentTxt: string) => {
+    try {
+      if (signedInUser) {
+        console.log(newComment, resourceData.id, signedInUser.id);
+        await axios.post(
+          url + `/comments/${signedInUser.id}/${resourceData.id}`,
+          {
+            text: commentTxt,
+          }
+        );
+      }
+    } catch (error) {
+      console.error("Woops... issue with POST request: ", error);
+    }
   };
 
   const handleFullViewClicked = () => {
@@ -67,11 +89,8 @@ function Resource({
     <div className="ctn-resource">
       <p className="resource-title">{resourceData.title}</p>
       <p className="resource-post-date">{String(resourceData.post_date)}</p>
-
       <p>{evaluateUsage(resourceData.usage)}</p>
-
       <p className="resource-type">{resourceData.type}</p>
-
       {/* -------------------------------if isFullView is true - render full description and all tags */}
       {isFullView ? (
         <>
@@ -123,11 +142,27 @@ function Resource({
         👀 Comments
       </button>
       {comments && (
-        <div>
-          {comments.map((el) => (
-            <p>{el.text}</p>
-          ))}
-        </div>
+        <>
+          {signedInUser && (
+            <div>
+              <input
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+              />
+              <button onClick={() => handleSubmitNewComment(newComment)}>
+                Submit Comment
+              </button>
+            </div>
+          )}
+          <div>
+            <h3>Comments</h3>
+            <ul>
+              {comments.map((el) => (
+                <li key={el.id}>{el.text}</li>
+              ))}
+            </ul>
+          </div>
+        </>
       )}
       <hr />
     </div>
