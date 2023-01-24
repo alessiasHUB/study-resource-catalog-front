81096e522562393a51f285e34c84adb567e4234c
Didier Osindero <didierosindero@gmail.com>
2023-01-24 14:16:06 +0000

    Formatted

--- src/components/CatalogPage.tsx
+++ src/components/CatalogPage.tsx
@@ -8,14 +8,13 @@ import { Link } from "react-router-dom";
 import "./CatalogPage.css";
 import { filterResources } from "../utils/filterResources";
 
-
 interface CatalogPageProps {
   signedInUser: IUserData | undefined;
 }
 
 function CatalogPage({ signedInUser }: CatalogPageProps): JSX.Element {
   const [resources, setResources] = useState<IResourceData[]>([]);
-  const [searchInput, setSearchInput] = useState<string>("")
+  const [searchInput, setSearchInput] = useState<string>("");
 
   const fetchAndStoreResources = useCallback(async () => {
     const response = await axios.get(`${url}/resources`);
@@ -29,33 +28,38 @@ function CatalogPage({ signedInUser }: CatalogPageProps): JSX.Element {
 
   return (
     <>
-    <div className="ctn-catalog-page-left">
-      <input className="catalog-search-input" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}  placeholder="search here..."></input>
-    </div>
-
-    <div className="ctn-catalog-page-right">
-      {signedInUser && (
-        <Link to="/add_resource" className="add-resource-btn">
-          Add new resource
-        </Link>
-      )}
-
-      <div className="ctn-resource-usage-key">
-        <p> Used and recommended = 🌟</p>
-        <p> Not used but recommended = 🔎</p>
-        <p>Not recommended = 💩</p>
+      <div className="ctn-catalog-page-left">
+        <input
+          className="catalog-search-input"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          placeholder="search here..."
+        ></input>
       </div>
-      {resources.length > 0 &&
-        filterResources(searchInput,resources).map((resource) => {
-          return (
-            <Resource
-              resourceData={resource}
-              signedInUser={signedInUser}
-              fetchAndStoreResources={fetchAndStoreResources}
-              key={resource.id}
-            />
-          );
-        })}
+
+      <div className="ctn-catalog-page-right">
+        {signedInUser && (
+          <Link to="/add_resource" className="add-resource-btn">
+            Add new resource
+          </Link>
+        )}
+
+        <div className="ctn-resource-usage-key">
+          <p> Used and recommended = 🌟</p>
+          <p> Not used but recommended = 🔎</p>
+          <p>Not recommended = 💩</p>
+        </div>
+        {resources.length > 0 &&
+          filterResources(searchInput, resources).map((resource) => {
+            return (
+              <Resource
+                resourceData={resource}
+                signedInUser={signedInUser}
+                fetchAndStoreResources={fetchAndStoreResources}
+                key={resource.id}
+              />
+            );
+          })}
       </div>
     </>
   );
